07447695e886b593fa390f0d3daf71ac1c42146c
vannshjindal <vanshjindal13@gmail.com>
2025-06-11 10:52:51 +0530

    feat: practiced bitwise operstors

--- fifth.js
+++ fifth.js
@@ -1,7 +1,26 @@
-var myFun = function(){
-    console.log("first");
-    return "third";
-}
+let a = "13";
+let b = 13;
+console.log(a === b); 
 
-console.log(myFun());
 
+
+let c = 12;
+let d = a++ ;
+let e = ++a;
+
+console.log(c); 
+console.log(d); 
+console.log(d); 
+
+
+
+let f = 11;
+let g = 22;
+let h = f + g + f++ + g++ + ++f + ++g;
+console.log(f); 
+console.log(g); 
+console.log(h); 
+
+let radius = 10;
+let circumferencofcircle = 2 * Math.PI * radius;
+console.log(circumferencofcircle);
